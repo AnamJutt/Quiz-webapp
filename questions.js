dd4f6c30fffc7b8d72e6a38aab1c4b961160b071
Anam Batool <saltbine@gmail.com>
2025-06-06 12:37:16 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        nmbr:1,
+        question:"What does HTML stands for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+           "A. Hyper Type Multi Language",
+           "B. Hyper Type Multiple Language",
+           "C. Hyper Text Markup Language",
+           "D. Home Type Multi Language"
+        ]
+    },
+    {
+        nmbr:2,
+        question:"What does CSS stands for?",
+        answer: "B. Cascading Style Sheets",
+        options: [
+            "A. Computer Style Sheets",
+            "B. Cascading Style Sheets",
+            "C. Creative Style System",
+            "D. Colorful Styling Structure"
+            
+        ]
+    },
+    {
+        nmbr:3,
+        question:"What does JS stands for?",
+        answer: "C. JavaScript",
+        options: [
+            "A. JavaSource ",
+            "B. JustScript",
+            "C. JavaScript ",
+            "D. JumpStyle "
+            
+        ]
+    },
+    {
+        nmbr:4,
+        question:"Which language is used to style a web page?",
+        answer: "B. CSS",
+        options: [
+            "A. HTML ",
+            "B. CSS",
+            "C. PYTHON ",
+            "D. JavaScript "
+            
+        ]
+    },
+    {
+        nmbr:5,
+        question:"What is the main purpose of a web browser?",
+        answer: "D. To display web pages",
+        options: [
+            "A. To write code ",
+            "B. To design images",
+            "C. To store data ",
+            "D. To display web pages "
+            
+        ]
+    }
+];
